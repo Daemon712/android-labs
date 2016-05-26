8efdc08906acabdcb83b6a5af759c31584366c47
Dmitriy <Daemon712@yandex.ru>
2016-05-27 00:39:41 +0300

    fix circle hit box

--- src/ru/foobarbaz/paint/shape/Circle.java
+++ src/ru/foobarbaz/paint/shape/Circle.java
@@ -4,29 +4,24 @@ import android.graphics.Canvas;
 
 public class Circle extends Shape {
 
-    private float centerX;
-    private float centerY;
-    private float radius;
-
     @Override
     public void draw(Canvas canvas) {
         float xMin = Math.min(startX, endX);
         float yMin = Math.min(startY, endY);
-        centerX = xMin + Math.abs(endX - startX) / 2;
-        centerY = yMin + Math.abs(endY - startY) / 2;
-        radius = (float) getDistance(centerX, centerY, endX, endY);
+        float centerX = xMin + Math.abs(endX - startX) / 2;
+        float centerY = yMin + Math.abs(endY - startY) / 2;
+        float radius = (float) getDistance(centerX, centerY, endX, endY);
         canvas.drawCircle(centerX, centerY, radius, paint);
     }
 
     @Override
     public boolean isHit(float x, float y) {
-        double xmax = Math.max(startX, endX);
-        double xmin = Math.min(startX, endX);
-        double ymax = Math.max(startY, endY);
-        double ymin = Math.min(startY, endY);
-        return (x >= xmin)
-                && (x <= xmax)
-                && (y >= ymin)
-                && (y <= ymax);
+        float xMin = Math.min(startX, endX);
+        float yMin = Math.min(startY, endY);
+        float centerX = xMin + Math.abs(endX - startX) / 2;
+        float centerY = yMin + Math.abs(endY - startY) / 2;
+        float radius = (float) getDistance(centerX, centerY, endX, endY);
+        float dist2point = (float) getDistance(centerX, centerY, x, y);
+        return dist2point < radius;
     }
 }
